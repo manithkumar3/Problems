90553d88d92a9a7cdb30519813579d926b32265c
Manith Kumar <manith.k@pacewisdom.com>
2026-02-23 15:10:53 +0530

    Implement add_two_numbers function to sum two linked lists representing non-negative integers

---
+++ add_two_numbers.py
@@ -0,0 +1,31 @@
+"""You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807."""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def add_two_numbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        num_1 = int("".join(str(n) for n in l1[::-1]))
+        num_2 = int("".join(str(n) for n in l2[::-1]))
+        result_num = str(num_1 + num_2)
+        return [int(x) for x in result_num]
